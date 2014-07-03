e066d1579c2563fa05f4b7d2d18da731841ba3e4
Henrique Ferreira <henriquebf@gmail.com>
2014-07-03 20:05:30 +0200

    Create Tasks

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+/*
+ * grunt-language-task
+ * https://github.com/henriquebf/grunt-language-task
+ *
+ * Copyright (c) 2014 Henrique Ferreira
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    language_task: {
+      options: {
+        languages: [
+          "de",
+          "en",
+          "pt"
+        ]
+      },
+      files: {
+        src: ['test/fixtures/*.json']
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'language_task', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/language_task.js
@@ -0,0 +1,137 @@
+//
+// grunt-language-task
+// https://github.com/henriquebf/grunt-language-task
+//
+// by Henrique Ferreira
+//
+
+'use strict';
+
+// Tasks Class Constructor
+
+function Tasks () {
+
+  // Default Options
+  this.default_options = {
+    languages: [
+      "de",
+      "en"
+    ]
+  };
+
+  // Bootstrap variables
+  this.content = {};
+  this.arrayErrors = [];
+
+}
+
+// Validate language Objects
+
+Tasks.prototype.validate = function (arrayLanguages) {
+
+  var self = this;
+  var isValid = true;
+
+  // Rotate languages to make sure we will check all keys in all languages
+  for (var activeIndex=0; activeIndex<arrayLanguages.length; activeIndex++) {
+
+    arrayLanguages = self.rotateArray(arrayLanguages);
+
+    // Check if language JSON file is present
+    arrayLanguages.forEach(function (locale) {
+      if (!self.content[locale]) {
+        isValid = false;
+        self.arrayErrors.push('Missing file ' + locale + '.json.');
+      }
+    });
+
+    // Iterate First Objects (and check the others)
+    for (var key_first in self.content[arrayLanguages[0]]) {
+      for (var key_second in self.content[arrayLanguages[0]][key_first]) {      
+        for (var i=1; i<arrayLanguages.length; i++) {
+          if(self.content[arrayLanguages[i]][key_first][key_second] === undefined) {
+            isValid = false;
+            self.arrayErrors.push('Missing content for "' + key_first + ' : ' + key_second + '" for locale "' + arrayLanguages[i] + '".');
+          }
+        }
+      }
+    }
+
+  }
+
+  // Remove duplicated error messages
+  self.arrayErrors = self.removeDuplicatedElements(self.arrayErrors);
+
+  return isValid
+
+};
+
+// Rotate Language Array [en,de,pt] -> [de,pt,en]
+
+Tasks.prototype.rotateArray = function (arrayLanguages) {
+  var newArray = [];
+  for (var i=0; i<arrayLanguages.length; i++) {
+    if (i>0) {
+      newArray[i-1] = arrayLanguages[i];
+    } else {
+      newArray[arrayLanguages.length-1] = arrayLanguages[i];
+    }
+  }
+  return newArray;
+};
+
+// Remove duplicated elements from Array
+
+Tasks.prototype.removeDuplicatedElements = function (arrayLanguages) {
+  var newArray = [];
+  var tempObject = {};
+  for (var i=0; i<arrayLanguages.length; i++) {
+    tempObject[arrayLanguages[i]] = arrayLanguages[i];
+  }
+  for (var key in tempObject) {
+    newArray.push(key);
+  }
+  return newArray;
+};
+
+// Get filename without extension from fullpath
+
+Tasks.prototype.getLocaleFromFilepath = function (filepath) {
+  return filepath.replace(/^.*[\\\/]/, '').replace('.json', '');
+};
+
+// Grunt Task
+
+module.exports = function (grunt) {
+
+  var LanguageTask = new Tasks;
+
+  grunt.registerMultiTask('language_task', 'An amazing Grunt Module to verify language JSON files before deployment', function () {
+
+    // Set default settings if not specified on Gruntfile.js
+    var options = this.options(LanguageTask.default_options);
+
+    // Iterate all language JSON files and place in the 'content' Object
+    this.files.forEach(function (file) {
+
+      // Read/Parse JSON files
+      var src = file.src.map(function (filepath) {
+        LanguageTask.content[LanguageTask.getLocaleFromFilepath(filepath)] = grunt.file.readJSON(filepath);
+        return true;
+      });
+
+      // Feedback the user with success message or error list
+      if(LanguageTask.validate(options.languages)) {
+        grunt.log.writeln('Language Task Complete!');
+      } else {
+        LanguageTask.arrayErrors.forEach(function (message) {
+          grunt.log.warn(message);
+        });
+        grunt.fail.warn("Validation Failed!");
+      }
+
+    });
+
+  });
+
+};
